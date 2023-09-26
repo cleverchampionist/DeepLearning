4f5678b694a5d3dfb755e45c6c9f509b41caa548
[cleverchampion] <[daniel.tarasuk.@outlook.com]>
2023-09-26 22:28:33 +0200

    first commit

--- dp.py
+++ dp.py
@@ -55,5 +55,35 @@ y_test = to_categorical(y_test, n_classes)
 #     print(y_train[i])
 
 history = model.fit(x_train, y_train, batch_size = 128,
-            epochs = 20, verbose = 2, validation_data = (x_test, y_test))
+            epochs = 10, verbose = 2, validation_data = (x_test, y_test))
+loss_and_metrics = model.evaluate(x_test, y_test, verbose = 2)
+
+print("Test Loss:", loss_and_metrics[0])
+print("Test Accuracy:", loss_and_metrics[1])
+
+plot.subplot(2, 1, 1)
+plot.plot(history.history['accuracy'])
+plot.plot(history.history['val_accuracy'])
+plot.title('model accuracy')
+plot.ylabel('accuracy')
+plot.xlabel('epoch')
+plot.legend(['train', 'test'], loc = 'lower right')
+
+plot.subplot(2, 1, 2)
+plot.plot(history.history['loss'])
+plot.plot(history.history['val_loss'])
+plot.title('model loss')
+plot.ylabel('loss')
+plot.xlabel('epoch')
+plot.legend(['train', 'test'], loc = 'upper right')
+plot.tight_layout()
+plot.show()
+
+predictions = model.predict(x_test)
+corret_predictions = np.nonzero(predictions == y_test)[0]
+incorrect_predictions = np.nonzero(predictions != y_test)[0]
+
+print(len(corret_predictions), "classified correctly")
+print(len(incorrect_predictions), "classified incorrectly")
+
 
