21c3b6b823ea580b064a2cc1f0f024de3ca4e4e1
[cleverchampion] <[daniel.tarasuk.@outlook.com]>
2023-10-07 08:45:26 +0200

    AI completed

--- AI/eye_detection.py
+++ AI/eye_detection.py
@@ -1,4 +1,15 @@
 import cv2
 import numpy as np
 
-eye_cascade = cv2.CascadeClassifier('hassrcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+img = cv2.imread('1.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+eyes = eye_cascade.detectMultiScale(gray, 1.03, 7)
+
+for (ex, ey, ew, eh) in eyes:
+    img = cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+cv2.imwrite('Eye_AB.jpg', img)
+cv2.imshow('eyes detection', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
